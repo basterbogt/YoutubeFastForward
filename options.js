dd3cfad884b27e3d5eb8ba9825a774ddeb5b5358
basterbogt <bas@basterbogt.com>
2019-05-18 23:53:28 +0200

    Sound lower while speed up
    Sound lower while speed up;
    Options expended with function to set the sound percentage based on the
    normal sound;
    Updated options code so that it will load the previous set options;
    Removed some un-used code;

--- options.js
+++ options.js
@@ -2,12 +2,75 @@
 
 'use strict';
 
-let input = document.getElementById('speedInput');
-function constructOptions(input) {
+let speedInput = document.getElementById('speedInput');
+let minimumSpeedValue = 0;
+let maximumSpeedValue = 16;
+let defaultSpeedStepSize = 0.25;
+
+let volumeInput = document.getElementById('volumeInput');
+let minimumVolumeValue = 0;
+let maximumVolumeValue = 100;
+let defaultVolumeStepSize = 5;
+
+function setSpeedInput(input) {
   input.oninput = function() {
       chrome.storage.sync.set({speed: input.value}, function() {
           console.log('Speed is ' + input.value);
       })
   };
 }
-constructOptions(input);
+
+function setVolumeInput(input) {
+  input.oninput = function() {
+      chrome.storage.sync.set({volume: (input.value / 100)}, function() {
+          console.log('Saved volume is ' + (input.value / 100));
+      })
+  };
+}
+
+function Init(){
+
+	speedInput.step = defaultSpeedStepSize;
+	speedInput.min = minimumSpeedValue;
+  speedInput.max = maximumSpeedValue;
+  
+	volumeInput.step = defaultVolumeStepSize;
+	volumeInput.min = minimumVolumeValue;
+  volumeInput.max = maximumVolumeValue;
+
+	document.addEventListener("wheel", function(e) {
+  });
+
+  chrome.storage.sync.get('speed', function(data) 
+  {
+    if(VariableIsSet(data.speed) && IsNumber(data.speed))
+    {
+      speedInput.value = data.speed;
+    }
+  });
+
+  chrome.storage.sync.get('volume', function(data) 
+  {
+    if(VariableIsSet(data.volume) && IsNumber(data.volume))
+    {
+      volumeInput.value = data.volume * 100;
+    }
+  });
+
+  setSpeedInput(speedInput);
+  setVolumeInput(volumeInput);
+}
+
+Init();
+
+//Helpers:
+
+function IsNumber(n) 
+{ 
+	return (!isNaN(parseFloat(n)) && !isNaN(n - 0));
+}
+
+function VariableIsSet(variableName)
+{
+	return (variableName !== undefined && variableName !== null);
+}
--- popup.js
+++ popup.js
@@ -8,73 +8,77 @@ let defaultSpeed = 1;
 let defaultSpeedStepSize = 0.25;
 let input = document.getElementById('speedInput');
 
+let defaultVolumeVariableName = 'defaultVolume';
+let speedUpVolume = 0.25; //25% is the default
+
 //Do this after page load:
 chrome.storage.sync.get('speed', function(data) 
 {
-	if(!VariableIsSet(data.speed) || !IsNumber(data.speed))
+	if(VariableIsSet(data.speed) && IsNumber(data.speed))
 	{
-		Init(defaultSpeed);
+		Init(data.speed);
 	}
 	else
 	{
-		Init(data.speed);
+		Init(defaultSpeed);
+	}
+});
+
+chrome.storage.sync.get('volume', function(data) 
+{
+	if(VariableIsSet(data.volume) && IsNumber(data.volume))
+	{
+		speedUpVolume = data.volume;
 	}
 });
 
 function Init(speed) 
 {  
+	GenerateDefaultVolumeVariableAtTab();
+
 	SetYoutubeSpeed(speed);
 
 	input.value = speed;
 	input.step = defaultSpeedStepSize;
 	input.min = minimumSpeedValue;
 	input.max = maximumSpeedValue;
-  	input.focus();
+	input.focus();
 
 	input.oninput = function() {
 		UpdateYoutubeAndCache(input.value);
-  	};
+	};
 
 	document.addEventListener("wheel", function(e) {
-	  	if (e.deltaY < 0) 
-	  	{
-	  		console.log("Scrolled up");
-	  	}
-	  	else if (e.deltaY > 0) 
-	  	{
-	  		console.log("Scrolled down");
-	  	}
 	});
 }
 
 //Functions:
+function GenerateDefaultVolumeVariableAtTab()
+{
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+		chrome.tabs.executeScript(
+				tabs[0].id,
+				{code: `${defaultVolumeVariableName} = document.getElementsByTagName("video")[0].volume;`});
+	});
+}
 
 function GetCurrentSpeed(){
 	return input.value;
-}
-
-function SetCurrentSpeed(speed)
-{
-	console.log(speed);
-	if(speed < minimumSpeedValue || speed > maximumSpeedValue)
-	{
-		return;
-	}
-
-	input.value = speed;
-	UpdateYoutubeAndCache(speed);
 }
 
 function UpdateYoutubeAndCache(speed)
 {
 	if(VariableIsSet(speed) && IsNumber(speed))
 	{
-  		SetYoutubeSpeed(speed);
-  		SaveToStorage(speed);
-  	}
-  	else{
-  		console.log("Not a number: " + speed);
-  	}
+		SetStepSize(speed);
+		SetYoutubeSpeed(speed);
+		SaveToStorage(speed);
+	}
+}
+
+function SetStepSize(speed)
+{
+	input.step = (speed >= 5)? defaultSpeedStepSize * 4 : defaultSpeedStepSize;
 }
 
 function SetYoutubeSpeed(speed)
@@ -84,10 +88,14 @@ function SetYoutubeSpeed(speed)
     	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 		    chrome.tabs.executeScript(
 		        tabs[0].id,
-		        {code: 'document.getElementsByTagName("video")[0].playbackRate = '+ speed +';document.getElementsByTagName("video")[0].play();'});
+						{code: `document.getElementsByTagName("video")[0].playbackRate = ${speed};` +
+									 `document.getElementsByTagName("video")[0].play();`+
+									 `document.getElementsByTagName("video")[0].volume = ${(speed > 1)? `(${defaultVolumeVariableName} * ${speedUpVolume})`: defaultVolumeVariableName};`});
 	  	});
 	}
 }
+
+//helpers:
 
 function IsNumber(n) 
 { 
@@ -99,16 +107,17 @@ function VariableIsSet(variableName)
 	return (variableName !== undefined && variableName !== null);
 }
 
-
 var storageSaveTimeout;
+
 function SaveToStorage(speed) 
 {
 	ResetSaveTimeout();
   	storageSaveTimeout = setTimeout(function(){ 
 	  	chrome.storage.sync.set({speed: speed}, null);
-	  	console.log("Saved setting speed: " + speed);
+	  	console.log(`Saved setting speed: ${speed}`);
   	}, 300);
 }
+
 function ResetSaveTimeout() 
 {
   clearTimeout(storageSaveTimeout);
